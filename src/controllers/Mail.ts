cc3f6f112a2c5b6aa45f52c1b81bb50fe922bfe6
cnvs_ikuji <k.ikuji@canvas-works.jp>
2025-01-22 16:41:23 +0900

    update resend mail text

--- src/controllers/Mail.ts
+++ src/controllers/Mail.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono"
-import {turnstile} from '@/middlewares/turnstile'
+import { turnstile } from '@/middlewares/turnstile'
 import { Resend } from "resend"
 import type { Bindings } from '@/index.ts'
 
@@ -7,17 +7,69 @@ const app = new Hono<{ Bindings: Bindings }>()
   .post('/send', turnstile, async (c) => {
     try {
       const body = await c.req.formData()
-      const [name, email] = [body.get("name"), body.get("email")]
-      if (typeof name !== "string" || typeof email !== "string") throw new Error("Name and email must be strings")
-
-      console.log(name, email)
+      const [name, email, kana, message] = [body.get("name"), body.get("kana"), body.get("email"), body.get("message")]
+      if (typeof name !== "string" || typeof kana !== "string" || typeof email !== "string" || typeof message !== "string") throw new Error("name, kana, email and message must be strings")
 
       const resend = new Resend(c.env.RESEND_API_KEY)
       const { data, error } = await resend.emails.send({
         from: c.env.EMAIL,
         to: [email],
-        subject: "Hello world",
-        text: `Welcome ${name}`,
+        subject: "【スタジオfirefly】お問い合わせありがとうございます",
+        text: `※このメールはシステムからの自動返信です
+
+${name} 様
+このたびはお問い合わせをいただき、誠にありがとうございます。 
+以下の内容でお問い合わせを受け付けいたしました。
+通常、3日以内にご連絡いたしますので、今しばらくお待ちくださいませ。
+
+─── お問い合わせ内容 ───
+お名前：${name}
+フリガナ：${kana}
+メールアドレス：${email}
+お問い合わせ内容：
+${message}
+───────────────────────
+
+尚、お問い合わせ内容により返信までにお時間をいただく場合がございます。
+あらかじめご了承くださいますようお願いいたします。
+
+# -- SIGNATURE --
+{
+  "name": "スタジオfirefly",
+  "contact": {
+    "email": "${c.env.EMAIL}",
+    "website": "https://studio-firefly.com",
+  }
+}
+# --------------`,
+      })
+
+      await resend.emails.send({
+        from: c.env.EMAIL,
+        to: c.env.EMAIL,
+        subject: "【スタジオfirefly】お問い合わせを受け付けました",
+        text: `※このメールはシステムからの自動返信です
+
+${name} 様より、以下の内容でお問い合わせを受け付けいたしました。
+3日以内にご連絡をお願いします。
+
+─── お問い合わせ内容 ───
+お名前：${name}
+フリガナ：${kana}
+メールアドレス：${email}
+お問い合わせ内容：
+${message}
+───────────────────────
+
+# -- SIGNATURE --
+{
+  "name": "スタジオfirefly",
+  "contact": {
+    "email": "${c.env.EMAIL}",
+    "website": "https://studio-firefly.com",
+  }
+}
+# --------------`,
       })
       return Response.json({ data, error })
 
