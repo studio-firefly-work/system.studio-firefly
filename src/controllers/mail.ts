9fe03000c5036f0730df824c7edf1b86f73fcd42
studio-firefly-work <account@studio-firefly.com>
2025-01-22 17:02:50 +0900

    del admin mail

--- src/controllers/mail.ts
+++ src/controllers/mail.ts
@@ -10,6 +10,8 @@ const app = new Hono<{ Bindings: Bindings }>()
       const [name, email, kana, message] = [body.get("name"), body.get("kana"), body.get("email"), body.get("message")]
       if (typeof name !== "string" || typeof kana !== "string" || typeof email !== "string" || typeof message !== "string") throw new Error("name, kana, email and message must be strings")
 
+      console.log(name, email, kana, message)
+
       const resend = new Resend(c.env.RESEND_API_KEY)
       const { data, error } = await resend.emails.send({
         from: c.env.EMAIL,
@@ -32,34 +34,6 @@ ${message}
 
 尚、お問い合わせ内容により返信までにお時間をいただく場合がございます。
 あらかじめご了承くださいますようお願いいたします。
-
-# -- SIGNATURE --
-{
-  "name": "スタジオfirefly",
-  "contact": {
-    "email": "${c.env.EMAIL}",
-    "website": "https://studio-firefly.com",
-  }
-}
-# --------------`,
-      })
-
-      await resend.emails.send({
-        from: c.env.EMAIL,
-        to: c.env.EMAIL,
-        subject: "【スタジオfirefly】お問い合わせを受け付けました",
-        text: `※このメールはシステムからの自動返信です
-
-${name} 様より、以下の内容でお問い合わせを受け付けいたしました。
-3日以内にご連絡をお願いします。
-
-─── お問い合わせ内容 ───
-お名前：${name}
-フリガナ：${kana}
-メールアドレス：${email}
-お問い合わせ内容：
-${message}
-───────────────────────
 
 # -- SIGNATURE --
 {
