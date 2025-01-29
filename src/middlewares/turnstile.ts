c2e8291be322e140b1670a063a93328f7a4afca3
studio-firefly-work <account@studio-firefly.com>
2025-01-29 16:57:52 +0900

    add turnstile verify

--- src/middlewares/turnstile.ts
+++ src/middlewares/turnstile.ts
@@ -1,11 +1,41 @@
 import { createMiddleware } from 'hono/factory'
-import turnstilePlugin from '@cloudflare/pages-plugin-turnstile'
+import type { TurnstileSuccess, TurnstileFailure } from '@cloudflare/pages-plugin-turnstile'
 
 export const turnstile = createMiddleware(async (c, next) => {
   try {
-    turnstilePlugin({ secret: c.env.TURNSTILE_SECRET_KEY })
+    // Turnstileのトークンを取得
+    const formData = await c.req.formData()
+    const token = formData.get('cf-turnstile-response') as string
+    const ip = c.req.header('CF-Connecting-IP') ?? ''
+
+    if (!token) {
+      console.log(`ip: ${ip}`)
+      return c.json('Turnstile token is missing', 400)
+    }
+
+    // Turnstileの検証リクエストを送信
+    const verificationUrl = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
+    const verificationBody = new URLSearchParams({
+      secret: c.env.TURNSTILE_SECRET_KEY,
+      response: token,
+      remoteip: ip,
+    });
+    const verifyResponse = await fetch(verificationUrl, {
+      method: 'POST',
+      body: verificationBody,
+    });
+    const verifyResponseJson: TurnstileSuccess | TurnstileFailure = await verifyResponse.json()
+
+    if (!verifyResponseJson.success) {
+      console.log(`error-codes: ${verifyResponseJson['error-codes']}`)
+      return c.json(verifyResponseJson, 403)
+    }
+
+    // 検証成功後、次のミドルウェアへ
     await next()
-  } catch (e: any) {
-    return new Response(e.message)
+
+  } catch (error: any) {
+    console.error(error)
+    return c.json(error.message, 500)
   }
 })
